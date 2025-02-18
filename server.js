4d41b47751c30df8d90d0ec2675f35c5a999af48
bataa_sme <bataa_sme@yahoo.com>
2025-02-18 17:58:30 +0800

    Fifth Commit

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.post('/api/create-invoice', async (req, res) => {
     const invoiceResponse = await axios.post(
       'https://merchant.qpay.mn/v2/invoice',
       {
-        invoice_code: "ACADEMIA_INVOICE",
+        invoice_code: "ACADEMIA_INVOICE1",
         sender_invoice_no: Date.now().toString(), // Unique invoice number
         invoice_receiver_code: "terminal",
         invoice_description: "academiacareer",
