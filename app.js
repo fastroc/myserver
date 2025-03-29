3542f77f7e737278eb4b8d2bdf8b9b1899924cec
bataa_sme <bataa_sme@yahoo.com>
2025-03-29 16:38:54 +0800

    Increased 10 mb

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: '10mb' }));
 
 let authToken = null;
 let tokenExpiration = 0;
